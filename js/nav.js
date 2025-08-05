d6e73d4d0e8c7f171d20b19566d68115f6ca0b81
messervolpe <mverni96@gmail.com>
2025-08-05 19:17:19 +0200

    moved js

---
+++ js/nav.js
@@ -0,0 +1,18 @@
+let btnNav = document.querySelector(".js-nav-btn");
+    let nav = document.querySelector(".js-nav");
+
+    if (window.innerWidth < 700) {
+    nav.classList.add("nav__list--hidden");
+    }
+
+    function toggleNav() {
+        nav.classList.toggle("nav__list--hidden");
+        if (btnNav.innerText == "+ menu") {
+            btnNav.innerText = "- menu";
+        }
+        else {
+            btnNav.innerText = "+ menu";
+        }
+    }
+
+    btnNav.addEventListener('click', toggleNav);
