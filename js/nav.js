d8cd2462cc9ce446ba69de1ae1943509838917e3
messervolpe <mverni96@gmail.com>
2025-08-12 14:59:02 +0200

    nav icons

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,5 @@
 let btnNav = document.querySelector(".js-nav-btn");
+let iconNav = document.querySelector(".js-iconNav")
 let nav = document.querySelector(".js-nav");
 
 if (window.innerWidth < 700) {
@@ -7,11 +8,11 @@ nav.classList.add("nav__list--hidden");
 
 function toggleNav() {
     nav.classList.toggle("nav__list--hidden");
-    if (btnNav.innerText == "+ menu") {
-        btnNav.innerText = "- menu";
+    if (iconNav.getAttribute('src') == "svg/menu-open.svg") {
+        iconNav.setAttribute('src', "svg/close.svg")
     }
     else {
-        btnNav.innerText = "+ menu";
+        iconNav.setAttribute('src', "svg/menu-open.svg")
     }
 }
 
