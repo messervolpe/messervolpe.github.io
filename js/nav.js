f40bd6b2bf4035f2076313ffee92eec3ee292dcd
messervolpe <mverni96@gmail.com>
2025-09-26 18:00:29 +0200

    add nav boolean

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 let btnNav = document.querySelector(".js-nav-btn");
 let iconNav = document.querySelector(".js-iconNav")
 let nav = document.querySelector(".js-nav");
+let nav_hidden = true;
 
 if (window.innerWidth < 700) {
 nav.classList.add("nav__list--hidden");
@@ -8,11 +9,13 @@ nav.classList.add("nav__list--hidden");
 
 function toggleNav() {
     nav.classList.toggle("nav__list--hidden");
-    if (iconNav.getAttribute('src') == "img/menu-open.svg") {
+    if (nav_hidden = true) {
         iconNav.setAttribute('src', "img/close.svg")
+        nav_hidden = false;
     }
     else {
         iconNav.setAttribute('src', "img/menu-open.svg")
+        nav_hidden = true;
     }
 }
 
