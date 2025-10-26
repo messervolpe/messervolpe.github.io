ffd36d0e2bde2ae5e9e249394c8ad9bf100a5b13
messervolpe <mverni96@gmail.com>
2025-10-26 17:44:09 +0100

    update archive

--- js/shapes.js
+++ js/shapes.js
@@ -1,31 +1,25 @@
 
-let Shape = document.querySelector(".js-path");
-const btnShape = document.querySelector(".js-create-shape");
-let selectCorners = document.querySelector(".js-corner-select")
-let corners = selectCorners.value
+const shape = document.querySelector(".js-path");
+const btnGenerate = document.querySelector(".js-create-shape");
+const selectCorners = document.querySelector(".js-corner-select");
+let nrCorners = selectCorners.value;
 
-function randomPairsArray(length, min, max) {
-  return Array.from({ length }, () => [
+
+function generateCordinates(length, min, max) {
+    return Array.from({length}, () => [
     Math.floor(Math.random() * (max - min + 1)) + min,
     Math.floor(Math.random() * (max - min + 1)) + min
-  ]);
-}
+  ])
+};
 
-function updateCorners() {
-  corners = selectCorners.value
-}
 
-function createShape() {
+selectCorners.addEventListener("change", () => {
+  nrCorners = selectCorners.value
+});
 
-    const newCoordinates = randomPairsArray(corners, 20, 300);
-    const newPathData = newCoordinates.map(pair => pair.join(' ')).join(' L ');
-
+btnGenerate.addEventListener("click", () => {
+    let newCoordinates = generateCordinates(nrCorners, 20, 300);
+    let newPathData = newCoordinates.map(pair => pair.join(' ')).join(' L ');
     newPath = `M ${newPathData} Z`;
-
-    Shape.setAttribute("d", newPath);
-}
-
-
-selectCorners.addEventListener("change", updateCorners);
-
-btnShape.addEventListener("click", createShape);
+    shape.setAttribute("d", newPath);
+} );
